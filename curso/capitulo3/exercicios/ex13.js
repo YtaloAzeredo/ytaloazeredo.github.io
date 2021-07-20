6544ce078dac4161dd18749593a628c88940de29
YtaloAzeredo <ytaloazeredo@gmail.com>
2021-07-20 02:21:33 -0300

    ex13 added, ex12 updated

---
+++ curso/capitulo3/exercicios/ex13.js
@@ -0,0 +1,10 @@
+function Fact()
+{
+var fatorial = prompt("Digite um numero ");
+var multipicador = fatorial - 1;
+for (var i = multipicador; i > 1; i--) 
+    {
+        fatorial *= i;
+    }
+alert("Resultado: "+fatorial);
+}
