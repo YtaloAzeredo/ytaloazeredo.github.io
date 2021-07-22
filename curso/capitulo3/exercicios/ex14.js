d5e04bcbde11188f16ba7070b4f9e196d23a4e33
YtaloAzeredo <ytaloazeredo@gmail.com>
2021-07-21 23:45:42 -0300

    ex14, ex15 added, summary updated

---
+++ curso/capitulo3/exercicios/ex14.js
@@ -0,0 +1,12 @@
+
+function Comparison()
+{
+    var dateIn = prompt("Insira uma data:");
+    var tp = new Date( Date.now() - new Date(dateIn) );
+    
+    var days = tp.getDate();
+    var months = tp.getMonth();
+    var years = tp.getFullYear() - 1970;
+
+    alert(years + " anos, " + months + " meses, " + days + " dias.");
+}
---
+++ curso/capitulo3/exercicios/ex15.js
@@ -0,0 +1,21 @@
+function Validator()
+{
+	var email = prompt("Digite um E-Mail:");
+
+	var arroba = email.split("@");
+
+    //alert(arroba.length);
+
+	var ponto = email.split(".");
+
+    //alert(ponto.lenght);
+
+ 	arroba.length == 2 && ponto.length == 2 ?
+	(
+		alert("E-mail válido.")
+    )
+	:
+	(
+		alert("E-mail Inválido")
+    )
+};
