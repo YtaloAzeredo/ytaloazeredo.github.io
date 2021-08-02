3b5d5a7094123a1d992f16afb3d9f7f1cda9f886
YtaloAzeredo <ytaloazeredo@gmail.com>
2021-08-02 07:04:08 -0300

    update p2

--- curso/capitulo3/p2.js
+++ curso/capitulo3/p2.js
@@ -37,31 +37,22 @@
 		var quartos = [0, 139, 220, 340];
 		var dias = verDias();
         var kid = 30;
-        
-
-		if(dias > 0)
-		{
-			var total = 0;
-            
-
-			for(i of document.getElementById("tipo-quarto").value)
-			{
-                
-				var subtotal = quartos[i];
-                
-                
-				for(j of document.getElementById("crianca").value)
-                {
-                    subtotal += kid*j;
-                }
-
-				total += subtotal * dias;
-			}
+        var i = document.getElementById("tipo-quarto").value
+		var j = document.getElementById("crianca").value
+		
+		if(dias > 0){
+			if (quartos[i] > 0){
+            kid = kid*j
+    
+			var total = (kid+quartos[i]) * dias;
             
 			alert("Reserva confirmada! \n\n Valor total: \n  R$" + total + ",00");
+			}
+			else{
+				alert("Entre com informações válidas!");
+			}
 		}
-		else
-		{
+		else{
 			alert("Entre com informações válidas!");
 		}
 	}
